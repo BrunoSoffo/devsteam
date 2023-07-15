5a2d971ecdfa5ccb7606742611ab00880d3d66d7
BrunoSoffo <brussoffo@gmail.com>
2023-07-15 02:43:13 -0300

    Logo, ícone e estilos

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,9 +1,13 @@
 import styles from './navbar.module.css'
+import { BsCart4} from 'react-icons/bs'
+import Logo from '@/components/logo/logo'
 
 export default function Navbar() {
     return(
-        <div>
-            Minha navbar
-        </div>
+        <nav className={styles.navbar}>
+            <Logo />
+            <input />
+            <BsCart4 size={40} />
+        </nav>
     )
 }
