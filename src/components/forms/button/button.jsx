52ebf731a6a66d6b23d91166bd70233dc3f77278
BrunoSoffo <brussoffo@gmail.com>
2023-07-15 21:03:21 -0300

    Game Card e alteração no botão

--- src/components/forms/button/button.jsx
+++ src/components/forms/button/button.jsx
@@ -1,7 +1,9 @@
 import styles from './button.module.css'
 
-export default function Button({children}){
+export default function Button({children, fullWidth}){
     return(
-        <button className={styles.button}>{children}</button>
-    )
+        <button className={`${styles.button} ${fullWidth && styles.fullwidth}`}>{children}</button>
+        // O "${...}" é uma sintaxe usada para interpolar uma expressão JavaScript em uma string
+        // ${fullWidth && styles.fullwidth}: Nesta parte, a variável fullWidth é uma propriedade booleana que determina se o botão deve ter largura total ou não. O && é o operador lógico "E" em JavaScript. Se fullWidth for verdadeiro, a classe styles.fullwidth será adicionada à lista de classes do botão. Caso contrário, essa classe não será incluída
+        )
 }
